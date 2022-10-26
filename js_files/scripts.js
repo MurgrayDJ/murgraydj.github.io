6dc9f6f066fae217b0bf041051300e08f63fb92a
murgraydj <murgraydj@gmail.com>
2022-10-26 17:44:51 -0400

    Updates navigation link target in scripts.js
    Instead of document.location.href, the location that's routed is
    window.location.href. Hopefully that will fix the navigation problem of
    the launched website.
    
    The navigation problem being: when a navbar button is clicked, the page
    is routed to murgray.com/<file>.html_files instead of
    murgray.com/html_files/<file>.html.

--- js_files/scripts.js
+++ js_files/scripts.js
@@ -1,13 +1,13 @@
 //Helper function
 function navigator(filename){
     if(document.URL.includes('index.html') && filename !== 'index.html'){
-        document.location.href = `./html_files/${filename}`;
+        window.location.href = `./html_files/${filename}`;
     }
     else if(!document.URL.includes('index.html') && filename === 'index.html'){
-        document.location.href = `../${filename}`;
+        window.location.href = `../${filename}`;
     }
     else{
-        document.location.href = `./${filename}`;
+        window.location.href = `./${filename}`;
     }
     
 }
